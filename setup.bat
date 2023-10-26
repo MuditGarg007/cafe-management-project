e9455e7813b83f0123e567871c3d2acd0176a641
Mudit <97163285+MuditGarg007@users.noreply.github.com>
2023-10-26 20:06:25 +0530

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Installing Colorama and mysql.connector Python modules...
+
+rem Use pip to install Colorama
+pip install colorama
+
+rem Use pip to install mysql.connector
+pip install mysql-connector-python
+
+echo Installation complete.
+pause
